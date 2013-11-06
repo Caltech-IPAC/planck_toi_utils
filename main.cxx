14625d5b90e38373a06b44cecf4b8b26f82ef005
Walter Landry <wlandry@caltech.edu>
2013-11-06 09:47:00 -0800

    Store result in per-search range and sum at end.  Still serial.

--- main.cxx
+++ main.cxx
@@ -9,19 +9,29 @@ int main(int argc, char *argv[])
     }
   try
     {
-      const int num_bins(40);
-      Ranges ranges(num_bins);
+      const size_t num_bins(40);
+      std::vector<Ranges> ranges;
+      for(int i=3;i<argc;++i)
+        {
+          ranges.emplace_back(num_bins);
+        }
 
       for(int i=3;i<argc;++i)
         {
           tinyhtm::Query query(argv[i],argv[1],argv[2]);
-          query.callback(std::bind(&Ranges::callback,&ranges,
+          query.callback(std::bind(&Ranges::callback,&ranges[i-3],
                                    std::placeholders::_1,
                                    std::placeholders::_2,
                                    std::placeholders::_3,
                                    std::placeholders::_4));
         }
-      std::cout << ranges;
+      Ranges result(num_bins);
+      for(auto &r: ranges)
+        {
+          for(size_t i=0;i<num_bins;++i)
+            result.r[i]+=r.r[i];
+        }
+      std::cout << result;
     }
   catch(std::runtime_error &e)
     {
