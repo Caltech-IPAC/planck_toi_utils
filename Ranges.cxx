f571c3c4c52b4657fe2bf9435daf64e392315431
Walter Landry <wlandry@caltech.edu>
2013-11-06 16:00:28 -0800

    Fix the start and end times and improve the error message

--- Ranges.cxx
+++ Ranges.cxx
@@ -1,9 +1,10 @@
 #include "Ranges.hxx"
 #include <cstring>
 
+//FIXME: Are these good numbers?
 /* Begin and end times for Planck mission */
-const uint64_t Ranges::begin_time=1705060000000000000;
-const uint64_t Ranges::end_time=1705160000000000000;
+const uint64_t Ranges::begin_time=1600000000000000000;
+const uint64_t Ranges::end_time=1750000000000000000;
 
 int Ranges::callback(void *entry, int num_elements, hid_t *, char **names)
 {
@@ -17,7 +18,10 @@ int Ranges::callback(void *entry, int num_elements, hid_t *, char **names)
           if(j>=r.size())
             {
               std::stringstream ss;
-              ss << "Bad time in database: " << utc;
+              ss << "Bad time in database: " << utc
+                 << "Allowed range: "
+                 << begin_time << " "
+                 << end_time;
               throw std::runtime_error(ss.str());
             }
           r[j]+=1;
