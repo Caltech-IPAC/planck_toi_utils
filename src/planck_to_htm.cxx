56a8a4d71e2e87cb7d027c2074f1bf17d3876bd1
Peregrine McGehee <peregrin@irsaphi1.ipac.caltech.edu>
2014-07-24 09:05:13 -0700

    repaired from 17 July merge commit

--- src/planck_to_htm.cxx
+++ src/planck_to_htm.cxx
@@ -196,121 +196,27 @@ int main (int argc, char *argv[])
               }
               catch (H5::Exception &e)
               {
-                H5::Exception::dontPrint();
-                H5::H5File file(path.string(), H5F_ACC_RDONLY);
-                H5::DataSet dataset;
-                H5::Group group;
-                try {
-                      group = file.openGroup("/");
-                      H5std_string sstr = group.getObjnameByIdx(0);
-                      std::cout << sstr.c_str() << "\n";
-                      dataset = file.openDataSet(sstr.c_str());
-
-                }
-                catch (H5::Exception &e)
-                {
-                    std::cout << "Warning: failed to find any dataset in "
-                              << "specified HDF5 file." << std::endl;
-                    continue;
-                }
+                std::cout << "Warning: failed to find any dataset in "
+                          << "specified HDF5 file." << std::endl;
+                continue;
+              }
 
 // Grab the attributes from the first successful file read
-                if (npoints == 0) {
-                    nattr = group.getNumAttrs();
-                    std::cout << "N attr = " << nattr << "\n";
-                    avec.resize(nattr);
-                    for (int i = 0; i < nattr; i++) {
-                       H5::Attribute a = group.openAttribute(i);
-                       std::cout << i << " " << a.getName() << "\n";
-                       avec[i] = a;
-                      }
-                }
-//
-                H5::DataSpace dataspace = dataset.getSpace();
-                hsize_t size;
-                dataspace.getSimpleExtentDims(&size, NULL);
-                npoints+=size;
-
-                planck_tod_entry foo1;
-                std::cout << "sizeof tod = " << sizeof(foo1) << "\n";
-
-                htm_entry<planck_tod_entry> foo2;
-                std::cout << "sizeof htm_entry<tod> = " << sizeof(foo2) << "\n";
-
-                std::cout << "npoints = " << npoints << "\n";
-
-                std::vector<planck_hdf5_entry> hdf_entries(size);
-                
-                H5::CompType compound(sizeof(planck_hdf5_entry));
-                compound.insertMember("glon",HOFFSET(planck_hdf5_entry,glon),
-                                      H5::PredType::NATIVE_FLOAT);
-                compound.insertMember("glat",HOFFSET(planck_hdf5_entry,glat),
-                                      H5::PredType::NATIVE_FLOAT);
-                compound.insertMember("psi",HOFFSET(planck_hdf5_entry,psi),
-                                      H5::PredType::NATIVE_FLOAT);
-                compound.insertMember("healpix_2048",
-                                      HOFFSET(planck_hdf5_entry,healpix_2048),
-                                      H5::PredType::NATIVE_INT32);
-                compound.insertMember("tsky",HOFFSET(planck_hdf5_entry,tsky),
-                                      H5::PredType::NATIVE_FLOAT);
-                compound.insertMember("utc",HOFFSET(planck_hdf5_entry,utc),
-                                      H5::PredType::NATIVE_DOUBLE);
-                compound.insertMember("sso",HOFFSET(planck_hdf5_entry,sso),
-                                      H5::PredType::NATIVE_UCHAR);
-
-                try {
-                    dataset.read(hdf_entries.data(), compound);
-                }
-                catch (H5::Exception &e)
-                {
-                    std::cout << "Warning:: Failed to read dataset in "
-                              << "specified HDF5 file." << std::endl;
-                }
-
-                long nentries(0);
-
-                for(auto &hdf_entry: hdf_entries)
-                  {
-                    nentries++;
-                    planck_tod_entry entry;
-
-                    entry.psi = hdf_entry.psi;
-                    const double MJD_1958_01_01=36204.0;
-                    entry.mjd = MJD_1958_01_01 + hdf_entry.utc / 86400.0;
-                    entry.tsky = hdf_entry.tsky;
-                    entry.sso = hdf_entry.sso;
-
-                    double ra=hdf_entry.glon;
-                    double dec=hdf_entry.glat;
-                    wcscon (WCS_GALACTIC, WCS_J2000, 0, 0, &ra, &dec, 1950);
-                    if(htm_sc_init(&entry.sc, ra, dec)!= HTM_OK)
-                      {
-                        std::stringstream ss;
-                        ss << "Bad latitude or longitude in record: "
-                           << " lon: " << hdf_entry.glon
-                           << " lat: " << hdf_entry.glat
-                           << "\n";
-                        throw std::runtime_error(ss.str());
-                      }
-                    struct htm_v3 v;
-                    if (htm_sc_tov3(&v, &entry.sc) != HTM_OK)
-                      {
-                        std::stringstream ss;
-                        ss << "Could not convert lon/lat to a vector in record: "
-                           << " lon: " << hdf_entry.glon
-                           << " lat: " << hdf_entry.glat
-                           << "\n";
-                        throw std::runtime_error(ss.str());
-                      }
-                    entry.htmid=htm_v3_id(&v,htm_depth);
-                    out.append(&entry);
-                  }
-                  std::cout << "# entries processed = " << nentries << "\n";
+              if (npoints == 0) {
+                  nattr = group.getNumAttrs();
+                  std::cout << "N attr = " << nattr << "\n";
+                  avec.resize(nattr);
+                  for (int i = 0; i < nattr; i++) {
+                     H5::Attribute a = group.openAttribute(i);
+                     std::cout << i << " " << a.getName() << "\n";
+                     avec[i] = a;
+                    }
               }
-              H5::DataSpace dataspace = dataset.getSpace ();
+//
+              H5::DataSpace dataspace = dataset.getSpace();
               hsize_t size;
-              dataspace.getSimpleExtentDims (&size, NULL);
-              npoints += size;
+              dataspace.getSimpleExtentDims(&size, NULL);
+              npoints+=size;
 
               planck_tod_entry foo1;
               std::cout << "sizeof tod = " << sizeof(foo1) << "\n";
@@ -320,105 +226,152 @@ int main (int argc, char *argv[])
 
               std::cout << "npoints = " << npoints << "\n";
 
-              std::vector<planck_hdf5_entry> hdf_entries (size);
+              std::vector<planck_hdf5_entry> hdf_entries(size);
+                
+              H5::CompType compound(sizeof(planck_hdf5_entry));
+              compound.insertMember("glon",HOFFSET(planck_hdf5_entry,glon),
+                                    H5::PredType::NATIVE_FLOAT);
+              compound.insertMember("glat",HOFFSET(planck_hdf5_entry,glat),
+                                    H5::PredType::NATIVE_FLOAT);
+              compound.insertMember("psi",HOFFSET(planck_hdf5_entry,psi),
+                                    H5::PredType::NATIVE_FLOAT);
+              compound.insertMember("healpix_2048",
+                                    HOFFSET(planck_hdf5_entry,healpix_2048),
+                                    H5::PredType::NATIVE_INT32);
+              compound.insertMember("tsky",HOFFSET(planck_hdf5_entry,tsky),
+                                    H5::PredType::NATIVE_FLOAT);
+              compound.insertMember("utc",HOFFSET(planck_hdf5_entry,utc),
+                                    H5::PredType::NATIVE_DOUBLE);
+              compound.insertMember("sso",HOFFSET(planck_hdf5_entry,sso),
+                                    H5::PredType::NATIVE_UCHAR);
 
-              H5::CompType compound (sizeof(planck_hdf5_entry));
-              compound.insertMember ("glon", HOFFSET (planck_hdf5_entry, glon),
-                                     H5::PredType::NATIVE_FLOAT);
-              compound.insertMember ("glat", HOFFSET (planck_hdf5_entry, glat),
-                                     H5::PredType::NATIVE_FLOAT);
-              compound.insertMember ("psi", HOFFSET (planck_hdf5_entry, psi),
-                                     H5::PredType::NATIVE_FLOAT);
-              compound.insertMember ("healpix_2048",
-                                     HOFFSET (planck_hdf5_entry, healpix_2048),
-                                     H5::PredType::NATIVE_INT32);
-              compound.insertMember ("tsky", HOFFSET (planck_hdf5_entry, tsky),
-                                     H5::PredType::NATIVE_FLOAT);
-              compound.insertMember ("utc", HOFFSET (planck_hdf5_entry, utc),
-                                     H5::PredType::NATIVE_DOUBLE);
-              compound.insertMember ("sso", HOFFSET (planck_hdf5_entry, sso),
-                                     H5::PredType::NATIVE_UCHAR);
-
-              try { dataset.read (hdf_entries.data (), compound); }
+              try {
+                  dataset.read(hdf_entries.data(), compound);
+              }
               catch (H5::Exception &e)
               {
-                std::cout << "Warning:: Failed to read dataset in "
-                          << "specified HDF5 file." << std::endl;
+                  std::cout << "Warning:: Failed to read dataset in "
+                            << "specified HDF5 file." << std::endl;
               }
 
-              long nentries (0);
+              long nentries(0);
 
-              for (auto &hdf_entry : hdf_entries)
+              for(auto &hdf_entry: hdf_entries)
                 {
                   nentries++;
                   planck_tod_entry entry;
 
                   entry.psi = hdf_entry.psi;
-                  const double MJD_1958_01_01 = 36204.0;
-                  entry.mjd = MJD_1958_01_01 + hdf_entry.utc / 1.0e9 / 86400.0;
+                  const double MJD_1958_01_01=36204.0;
+                  entry.mjd = MJD_1958_01_01 + hdf_entry.utc / 86400.0;
                   entry.tsky = hdf_entry.tsky;
                   entry.sso = hdf_entry.sso;
 
-                  double ra = hdf_entry.glon;
-                  double dec = hdf_entry.glat;
+                  double ra=hdf_entry.glon;
+                  double dec=hdf_entry.glat;
                   wcscon (WCS_GALACTIC, WCS_J2000, 0, 0, &ra, &dec, 1950);
-                  if (htm_sc_init (&entry.sc, ra, dec) != HTM_OK)
+                  if(htm_sc_init(&entry.sc, ra, dec)!= HTM_OK)
                     {
                       std::stringstream ss;
                       ss << "Bad latitude or longitude in record: "
                          << " lon: " << hdf_entry.glon
-                         << " lat: " << hdf_entry.glat << "\n";
-                      throw std::runtime_error (ss.str ());
+                         << " lat: " << hdf_entry.glat
+                         << "\n";
+                      throw std::runtime_error(ss.str());
                     }
                   struct htm_v3 v;
-                  if (htm_sc_tov3 (&v, &entry.sc) != HTM_OK)
+                  if (htm_sc_tov3(&v, &entry.sc) != HTM_OK)
                     {
                       std::stringstream ss;
                       ss << "Could not convert lon/lat to a vector in record: "
                          << " lon: " << hdf_entry.glon
-                         << " lat: " << hdf_entry.glat << "\n";
-                      throw std::runtime_error (ss.str ());
+                         << " lat: " << hdf_entry.glat
+                         << "\n";
+                      throw std::runtime_error(ss.str());
                     }
-                  entry.htmid = htm_v3_id (&v, htm_depth);
-                  out.append (&entry);
-                }
-              std::cout << "# entries processed = " << nentries << "\n";
-            }
-          else
+                  entry.htmid=htm_v3_id(&v,htm_depth);
+                  out.append(&entry);
+             }
+            std::cout << "# entries processed = " << nentries << "\n";
+             
+            try { dataset.read (hdf_entries.data (), compound); }
+            catch (H5::Exception &e)
             {
-              throw std::runtime_error ("Unknown file format: "
-                                        + path.string ());
+              std::cout << "Warning:: Failed to read dataset in "
+                        << "specified HDF5 file." << std::endl;
             }
-        }
+
+            for (auto &hdf_entry : hdf_entries)
+              {
+                nentries++;
+                planck_tod_entry entry;
+
+                entry.psi = hdf_entry.psi;
+                const double MJD_1958_01_01 = 36204.0;
+                entry.mjd = MJD_1958_01_01 + hdf_entry.utc / 86400.0;
+                entry.tsky = hdf_entry.tsky;
+                entry.sso = hdf_entry.sso;
+
+                double ra = hdf_entry.glon;
+                double dec = hdf_entry.glat;
+                wcscon (WCS_GALACTIC, WCS_J2000, 0, 0, &ra, &dec, 1950);
+                if (htm_sc_init (&entry.sc, ra, dec) != HTM_OK)
+                  {
+                    std::stringstream ss;
+                    ss << "Bad latitude or longitude in record: "
+                       << " lon: " << hdf_entry.glon
+                       << " lat: " << hdf_entry.glat << "\n";
+                    throw std::runtime_error (ss.str ());
+                  }
+                struct htm_v3 v;
+                if (htm_sc_tov3 (&v, &entry.sc) != HTM_OK)
+                  {
+                    std::stringstream ss;
+                    ss << "Could not convert lon/lat to a vector in record: "
+                       << " lon: " << hdf_entry.glon
+                       << " lat: " << hdf_entry.glat << "\n";
+                    throw std::runtime_error (ss.str ());
+                  }
+                entry.htmid = htm_v3_id (&v, htm_depth);
+                out.append (&entry);
+              }
+            std::cout << "# entries processed = " << nentries << "\n";
+          }
+        else
+          {
+            throw std::runtime_error ("Unknown file format: "
+                                      + path.string ());
+          }
+       }
     }
 
     std::chrono::milliseconds msecs (
         std::chrono::duration_cast<std::chrono::milliseconds>(
-            std::chrono::high_resolution_clock::now () - start_time));
+        std::chrono::high_resolution_clock::now () - start_time));
     std::cout << "Read files "
-              << "\t" << msecs.count () << " msec total\n";
+            << "\t" << msecs.count () << " msec total\n";
 
-      const size_t minpoints = 1024;
-      const uint64_t leafthresh = 64;
-      sort_and_index<planck_tod_entry>(data_file, scratch_file,
-                                       htm_file,mem,npoints,
-                                       minpoints,leafthresh);
+    const size_t minpoints = 1024;
+    const uint64_t leafthresh = 64;
+    sort_and_index<planck_tod_entry>(data_file, scratch_file,
+                                     htm_file,mem,npoints,
+                                     minpoints,leafthresh);
 // Add attributes to indexed file.
-      std::cout << "\nAdding attributes... ";
-      H5::H5File file2(data_file, H5F_ACC_RDWR);
-      H5::Group group2;
-      group2 = file2.openGroup("/");     
-      H5::Attribute att;
-      std::string str;  	
-      for (int i = 0; i < nattr; i++) {
-          att = group2.createAttribute(avec[i].getName(), 
-                                 avec[i].getDataType(),
-                                 avec[i].getSpace());
-          avec[i].read(avec[i].getDataType(), str);
-          att.write(avec[i].getDataType(), str);
-      }
-      std::cout << " done.\n";
+    std::cout << "\nAdding attributes... ";
+    H5::H5File file2(data_file, H5F_ACC_RDWR);
+    H5::Group group2;
+    group2 = file2.openGroup("/");     
+    H5::Attribute att;
+    std::string str;  	
+    for (int i = 0; i < nattr; i++) {
+        att = group2.createAttribute(avec[i].getName(), 
+                               avec[i].getDataType(),
+                               avec[i].getSpace());
+        avec[i].read(avec[i].getDataType(), str);
+        att.write(avec[i].getDataType(), str);
     }
+    std::cout << " done.\n";
+  }
   catch (H5::Exception &e)
   {
     std::cerr << "HDF5 Exception: " << e.getDetailMsg () << "\n";
